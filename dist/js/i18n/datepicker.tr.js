8cf405f69eff1940649155db16c83e3f8bdbed73
marblingweb <50891652+marblingweb@users.noreply.github.com>
2019-05-22 02:28:44 +0300

    datepicker.tr.js

---
+++ dist/js/i18n/datepicker.tr.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['tr'] = {
+    days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+    daysShort: ['Paz', 'Pzt', 'Sal', 'Çar', 'Per', 'Cum', 'Cts'],
+    daysMin: ['Pz', 'Pt', 'Sa', 'Ça', 'Pe', 'Cu', 'Ct'],
+    months: ['Ocak','Şubat','Mart','Nisan','Mayıs','Haziran', 'Temmuz','Ağustos','Eylül','Ekim','Kasım','Aralık'],
+    monthsShort: ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+    today: 'Bugün',
+    clear: 'Temizle',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
